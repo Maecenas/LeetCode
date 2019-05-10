74093f9f8c736014bdc751d66027257f4b8ca183
Maecenas Li <lx70716@gmail.com>
2019-05-10 20:50:16 +0900

    🚚 feat: Backup #27 #28 #35 #38 #53

---
+++ src/main/java/LeetCode/_28_ImplementStrStr.java
@@ -0,0 +1,51 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/implement-strstr/
+Easy. Two Pointers, String.
+
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string.
+This is consistent to C's strstr() and Java's indexOf().
+*/
+
+/**
+ * Substring Search can be made in linear/ sub-linear time with algorithms like
+ * Knuth-Morris-Pratt, Boyer-Moore and Rabin-Karp. See also:
+ * <a>https://algs4.cs.princeton.edu/53substring/</a>
+ * <p>
+ * Such type of questions can be extended as Pattern Matching
+ */
+class _28_ImplementStrStr {
+
+    public static int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null || needle.equals("")) return 0;
+
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            // no exit condition means something to dealt with on end
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/LeetCode/_53_MaximumSubarray.java
@@ -0,0 +1,65 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/maximum-subarray/
+Easy. Array, Divide and Conquer, Dynamic Programming.
+
+Given an integer array nums, find the contiguous subarray (containing at least one number)
+which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Follow up:
+
+If you have figured out the O(n) solution,
+try coding another solution using the divide and conquer approach, which is more subtle.
+*/
+
+class _53_MaximumSubarray {
+
+    public static int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return Integer.MIN_VALUE;
+
+        int res = Integer.MIN_VALUE, sum = 0;
+        for (int num : nums) {
+            sum = Math.max(sum, 0) + num;
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
+
+    public static int maxSubArrayDivideAndConquer(int[] nums) {
+        if (nums == null || nums.length == 0) return Integer.MIN_VALUE;
+
+        return maxSubArray(nums, 0, nums.length - 1);
+    }
+
+    private static int maxSubArray(final int[] nums, final int lo, final int hi) {
+        if (lo > hi) return Integer.MIN_VALUE;
+        if (lo == hi) return nums[lo];
+
+        // StackOverflowError
+        // final int mid = lo + (hi - lo) >> 1;
+        final int mid = (lo + hi) >> 1;
+        final int L = maxSubArray(nums, lo, mid - 1);
+        final int R = maxSubArray(nums, mid + 1, hi);
+
+        int currSum = 0;
+        int leftSum = 0;
+        for (int i = mid - 1; i >= lo; i--) {
+            currSum += nums[i];
+            leftSum = Math.max(leftSum, currSum);
+        }
+        currSum = 0;
+        int rightSum = 0;
+        for (int i = mid + 1; i <= hi; i++) {
+            currSum += nums[i];
+            rightSum = Math.max(rightSum, currSum);
+        }
+        return Math.max(Math.max(L, R), leftSum + rightSum + nums[mid]);
+    }
+}
