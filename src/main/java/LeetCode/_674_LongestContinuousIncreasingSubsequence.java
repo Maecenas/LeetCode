cef9b8dabe9b7d0c79a39686b93f2879b698eed3
Maecenas Li <lx70716@gmail.com>
2019-05-17 23:55:44 +0900

    🚚 feat: Backup #661 #665 #674 #697 #717

---
+++ src/main/java/LeetCode/_674_LongestContinuousIncreasingSubsequence.java
@@ -0,0 +1,45 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+Easy. Array.
+
+Given an unsorted array of integers, find the length of longest continuous
+increasing subsequence (subarray).
+
+Example 1:
+
+Input: [1,3,5,4,7]
+Output: 3
+Explanation: The longest continuous increasing subsequence is [1,3,5], its length is 3.
+Even though [1,3,5,7] is also an increasing subsequence, it's not a continuous one
+where 5 and 7 are separated by 4.
+
+Example 2:
+
+Input: [2,2,2,2,2]
+Output: 1
+Explanation: The longest continuous increasing subsequence is [2], its length is 1.
+
+Note:
+Length of the array will not exceed 10,000.
+*/
+
+class _674_LongestContinuousIncreasingSubsequence {
+
+    public static int findLengthOfLCIS(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        else if (nums.length == 1) return 1;
+
+        int res = 1, count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                count++;
+                res = Math.max(res, count);
+            } else {
+                count = 1;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/main/java/LeetCode/_697_DegreeOfAnArray.java
@@ -0,0 +1,79 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/degree-of-an-array/
+Easy. Array.
+
+Given a non-empty array of non-negative integers nums, the degree of this
+array is defined as the maximum frequency of any one of its elements.
+Your task is to find the smallest possible length of a (contiguous)
+subarray of nums, that has the same degree as nums.
+
+Example 1:
+
+Input: [1, 2, 2, 3, 1]
+Output: 2
+Explanation:
+The input array has a degree of 2 because both elements 1 and 2 appear twice.
+Of the subarrays that have the same degree:
+[1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+The shortest length is 2. So return 2.
+
+Example 2:
+
+Input: [1,2,2,3,1,4,2]
+Output: 6
+
+Note:
+nums.length will be between 1 and 50,000.
+nums[i] will be an integer between 0 and 49,999.
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+class _697_DegreeOfAnArray {
+
+    public static int findShortestSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        else if (nums.length == 1) return 1;
+
+        //final HashMap<Integer, Integer> counter = new HashMap<>(), first = new HashMap<>();
+        //int res = 0, degree = 0;
+        //for (int i = 0; i < nums.length; i++) {
+        //    //first.putIfAbsent(nums[i], i);
+        //    if (!first.containsKey(nums[i])) {
+        //        first.put(nums[i], i);
+        //    }
+        //    counter.put(nums[i], counter.getOrDefault(nums[i], 0) + 1);
+        //    if (counter.get(nums[i]) > degree) {
+        //        degree = counter.get(nums[i]);
+        //        res = i - first.get(nums[i]) + 1;
+        //    } else if (counter.get(nums[i]) == degree) {
+        //        res = Math.min(res, i - first.get(nums[i]) + 1);
+        //    }
+        //}
+
+        // the first 16 bits of entry are used to store first appearing index
+        // the last 16 bits are used to count num
+        final HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0, degree = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], i << 16);
+            } else {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+
+            int count = map.get(nums[i]) & 0xFFFF;
+            if (count > degree) {
+                degree = count;
+                res = i - (map.get(nums[i]) >> 16) + 1;
+            } else if (count == degree) {
+                res = Math.min(res, i - (map.get(nums[i]) >> 16) + 1);
+            }
+        }
+        return res;
+    }
+}
