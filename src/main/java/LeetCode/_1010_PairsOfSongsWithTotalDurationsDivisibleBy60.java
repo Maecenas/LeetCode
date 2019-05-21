4c9bbddc36a6854fca83ff218e6b83c385df0468
Maecenas Li <lx70716@gmail.com>
2019-05-22 07:26:31 +0900

    ✨ feat: Add #999 #1002 #1010 #1013 #1018

---
+++ src/main/java/LeetCode/_1010_PairsOfSongsWithTotalDurationsDivisibleBy60.java
@@ -0,0 +1,74 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+Easy. Array.
+
+In a list of songs, the i-th song has a duration of time[i] seconds.
+
+Return the number of pairs of songs for which their total duration in seconds
+is divisible by 60. Formally, we want the number of indices i < j
+with (time[i] + time[j]) % 60 == 0.
+
+Example 1:
+
+Input: [30,20,150,100,40]
+Output: 3
+Explanation: Three pairs have a total duration divisible by 60:
+(time[0] = 30, time[2] = 150): total duration 180
+(time[1] = 20, time[3] = 100): total duration 120
+(time[1] = 20, time[4] = 40): total duration 60
+
+Example 2:
+
+Input: [60,60,60]
+Output: 3
+Explanation: All three pairs have a total duration of 120, which is divisible by 60.
+
+Note:
+
+1 <= time.length <= 60000
+1 <= time[i] <= 500
+*/
+
+class _1010_PairsOfSongsWithTotalDurationsDivisibleBy60 {
+
+    private static final int R = 60;  // given divisor
+
+    public static int numPairsDivisibleBy60(int[] time) {
+        if (time == null || time.length <= 1) return 0;
+
+        final int[] count = new int[R];
+        for (int t : time) {
+            count[t % R]++;
+        }
+
+        int res = 0;
+        for (int i = 0; i <= (R >> 1); i++) {
+            if (i == 0 || (i << 1) == R) {
+                // from n choose r: (n * (n - 1)) / 2
+                res += (count[i] * (count[i] - 1)) >> 1;
+            } else {
+                res += (count[i] * count[R - i]);
+            }
+        }
+        return res;
+    }
+
+    /**
+     * One-pass solution that is even easier to write
+     */
+    public static int numPairsDivisibleBy60OnePass(int[] time) {
+        if (time == null || time.length <= 1) return 0;
+
+        final int[] count = new int[R];
+        int res = 0;
+        for (int t : time) {
+            t %= R;
+            // add % R in case that t == 0
+            res += count[(R - t) % R];
+            count[t % R]++;
+        }
+        return res;
+    }
+}
---
+++ src/main/java/LeetCode/_1013_PartitionArrayIntoThreePartsWithEqualSum.java
@@ -0,0 +1,62 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/partition-array-into-three-parts-with-equal-sum/
+Easy. Array.
+
+Given an array A of integers, return true if and only if
+we can partition the array into three non-empty parts with equal sums.
+
+Formally, we can partition the array if we can find indexes i+1 < j with
+(A[0] + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1])
+
+Example 1:
+
+Input: [0,2,1,-6,6,-7,9,1,2,0,1]
+Output: true
+Explanation: 0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+
+Example 2:
+
+Input: [0,2,1,-6,6,7,9,-1,2,0,1]
+Output: false
+
+Example 3:
+
+Input: [3,3,6,5,-2,2,5,1,-9,4]
+Output: true
+Explanation: 3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+
+Note:
+
+3 <= A.length <= 50000
+-10000 <= A[i] <= 10000
+*/
+
+class _1013_PartitionArrayIntoThreePartsWithEqualSum {
+
+    public static boolean canThreePartsEqualSum(int[] A) {
+        if (A == null || A.length < 3) return false;
+
+        int sum = 0;
+        for (int i : A) {
+            sum += i;
+        }
+        // early return
+        if (sum % 3 != 0) return false;
+        final int partSum = sum / 3;
+
+        int currSum = 0, count = 0;
+        for (int i : A) {
+            currSum += i;
+            if (currSum == partSum) {
+                count++;
+                if (count == 2) {
+                    return true;
+                }
+                currSum = 0;
+            }
+        }
+        return false;
+    }
+}
