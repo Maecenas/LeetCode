a13ae0e9cd77c19e8884259ba9e2a9bd60f91142
Maecenas Li <lx70716@gmail.com>
2019-06-01 14:36:58 +0900

    ✨ feat: Add #526 #560 $565 #611 #667

---
+++ src/main/java/LeetCode/_526_BeautifulArrangement.java
@@ -0,0 +1,58 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/beautiful-arrangement/
+Medium. Backtracking.
+
+Suppose you have N integers from 1 to N. We define a beautiful arrangement
+as an array that is constructed by these N numbers successfully if one of
+the following is true for the ith position (1 <= i <= N) in this array:
+
+The number at the ith position is divisible by i.
+i is divisible by the number at the ith position.
+
+Now given N, how many beautiful arrangements can you construct?
+
+Example 1:
+
+Input: 2
+Output: 2
+
+Explanation:
+
+The first beautiful arrangement is [1, 2]:
+Number at the 1st position (i=1) is 1, and 1 is divisible by i (i=1).
+Number at the 2nd position (i=2) is 2, and 2 is divisible by i (i=2).
+
+The second beautiful arrangement is [2, 1]:
+Number at the 1st position (i=1) is 2, and 2 is divisible by i (i=1).
+Number at the 2nd position (i=2) is 1, and i (i=2) is divisible by 1.
+
+Note:
+
+N is a positive integer and will not exceed 15.
+*/
+
+/**
+ * @see _667_BeautifulArrangementII
+ */
+class _526_BeautifulArrangement {
+
+    public static int countArrangement(int N) {
+        return countArrangement(N, new boolean[N + 1], 1, 1);
+    }
+
+    private static int countArrangement(final int N, final boolean[] visited, int count, int idx) {
+        if (idx > N) {
+            count++;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && (idx % i == 0 || i % idx == 0)) {
+                visited[i] = true;
+                count = countArrangement(N, visited, count, idx + 1);
+                visited[i] = false;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/main/java/LeetCode/_565_ArrayNesting.java
@@ -0,0 +1,75 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/array-nesting/
+Medium. Array.
+
+A zero-indexed array A of length N contains all integers from 0 to N-1.
+Find and return the longest length of set S, where S[i] =
+{A[i], A[A[i]], A[A[A[i]]], ... } subjected to the rule below.
+
+Suppose the first element in S starts with the selection of element A[i]
+of index = i, the next element in S should be A[A[i]], and then A[A[A[i]]]…
+By that analogy, we stop adding right before a duplicate element occurs in S.
+
+Example 1:
+
+Input: A = [5,4,0,3,1,6,2]
+Output: 4
+Explanation:
+A[0] = 5, A[1] = 4, A[2] = 0, A[3] = 3, A[4] = 1, A[5] = 6, A[6] = 2.
+
+One of the longest S[K]:
+S[0] = {A[0], A[5], A[6], A[2]} = {5, 6, 2, 0}
+
+Note:
+
+N is an integer within the range [1, 20,000].
+The elements of A are all distinct.
+Each element of A is an integer within the range [0, N - 1].
+*/
+
+class _565_ArrayNesting {
+
+    public static int arrayNesting(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        else if (nums.length == 1) return 1;
+
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == -1) continue;
+
+            int count = 0;
+            int start = nums[i];
+            while (nums[start] != -1) {
+                int next = nums[start];
+                count++;
+                nums[start] = -1;
+                start = next;
+            }
+            res = Math.max(res, count);
+        }
+        return res;
+    }
+
+    public static int arrayNesting2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        else if (nums.length == 1) return 1;
+
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] >> 15) != 0x0) continue;
+
+            int count = 0;
+            int start = nums[i];
+            while ((nums[start] >> 15) == 0) {
+                int next = nums[start];
+                count++;
+                nums[start] |= 0x8000;  // 1 << 15
+                start = next;
+            }
+            res = Math.max(res, count);
+        }
+        return res;
+    }
+}
