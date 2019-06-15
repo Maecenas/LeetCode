16da84148c574e11ca47d7006090cecaa1e5fc34
Maecenas Li <lx70716@gmail.com>
2019-06-15 13:59:02 +0900

    ✨ feat: Add #792 #795 #825 #835 #870

---
+++ src/main/java/LeetCode/_795_NumberOfSubarraysWithBoundedMaximum.java
@@ -0,0 +1,50 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/number-of-subarrays-with-bounded-maximum/
+Medium. Array.
+
+We are given an array A of positive integers,
+and two positive integers L and R (L <= R).
+
+Return the number of (contiguous, non-empty) subarrays such that the value of
+the maximum array element in that subarray is at least L and at most R.
+
+Example:
+Input:
+A = [2, 1, 4, 3]
+L = 2
+R = 3
+Output: 3
+Explanation: There are three subarrays that meet the requirements:
+[2], [2, 1], [3].
+
+Note:
+
+L, R and A[i] will be an integer in the range [0, 10^9].
+The length of A will be in the range of [1, 50000].
+*/
+
+class _795_NumberOfSubarraysWithBoundedMaximum {
+
+    public static int numSubarrayBoundedMax(int[] A, int L, int R) {
+        if (A == null || A.length == 0 || L > R) return 0;
+
+        int start = 0, count = 0, res = 0;
+
+        for (int end = 0; end < A.length; end++) {
+            if (A[end] >= L && A[end] <= R) {
+                count = end - start + 1;
+                res += count;
+            } else if (A[end] < L) {
+                // count([start, end]) = count([start, end - 1])
+                res += count;
+            } else {
+                // A[end] > R
+                start = end + 1;
+                count = 0;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/main/java/LeetCode/_870_AdvantageShuffle.java
@@ -0,0 +1,224 @@
+package LeetCode;
+
+/*
+https://leetcode.com/problems/advantage-shuffle/
+Medium. Array, Greedy.
+
+Given two arrays A and B of equal size, the advantage of A with respect to B
+is the number of indices i for which A[i] > B[i].
+
+Return any permutation of A that maximizes its advantage with respect to B.
+
+Example 1:
+
+Input: A = [2,7,11,15], B = [1,10,4,11]
+Output: [2,11,7,15]
+
+Example 2:
+
+Input: A = [12,24,8,32], B = [13,25,32,11]
+Output: [24,32,8,12]
+
+Note:
+
+1 <= A.length = B.length <= 10000
+0 <= A[i] <= 10^9
+0 <= B[i] <= 10^9
+*/
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.TreeMap;
+
+class _870_AdvantageShuffle {
+
+    /**
+     * O(nlogn), O(n)
+     * Using Tian Ji Horse-racing strategy
+     * <p>
+     * Other solutions without sorting B will fail if there are duplicate elements in B,
+     * because we're using elements as keys and a hashtable can not have duplicate
+     * keys with different values
+     */
+    public static int[] advantageCount(int[] A, int[] B) {
+        if (A == null || B == null || A.length != B.length) return new int[0];
+
+        final int len = A.length;
+
+        final int[] sortedA = Arrays.copyOf(A, len), sortedB = Arrays.copyOf(B, len);
+        Arrays.sort(sortedA);
+        Arrays.sort(sortedB);
+
+        // assigned[b] = A's elements that are assigned to beat b
+        final HashMap<Integer, Queue<Integer>> assigned = new HashMap<>();
+        for (int b : B) {
+            assigned.putIfAbsent(b, new LinkedList<>());
+        }
+
+        // remaining = A's element that are not assigned to any b
+        final Queue<Integer> remained = new LinkedList<>();
+
+        // populate (assigned, remaining) appropriately
+        // sortedB[j] is always the smallest unassigned element in B
+        int j = 0;
+        for (int a : sortedA) {
+            if (a > sortedB[j]) {
+                assigned.get(sortedB[j]).offer(a);
+                j++;
+            } else {
+                remained.offer(a);
+            }
+        }
+
+        final int[] res = new int[len];
+        for (int i = 0; i < len; i++) {
+            // if there is some a assigned to b
+            Integer x = assigned.get(B[i]).poll();
+            if (x == null) x = remained.poll();
+            // assert x != null;
+            res[i] = x;
+        }
+        return res;
+    }
+
+    /**
+     * Virtually sort B by an array to record the index of B after sorting
+     */
+    public static int[] advantageCount2(int[] A, int[] B) {
+        if (A == null || B == null || A.length != B.length) return new int[0];
+
+        Arrays.sort(A);
+
+        final int len = A.length;
+        final Integer[] bIndex = new Integer[len];
+        for (int i = 0; i < len; i++) {
+            bIndex[i] = i;
+        }
+        Arrays.sort(bIndex, Comparator.comparingInt(i -> B[i]));
+
+        final int[] res = new int[len];
+        int start = 0, end = len - 1;
+        for (int idxA = 0, idxB = 0; idxA < len; idxA++) {
+            if (A[idxA] > B[bIndex[idxB]]) {
+                res[bIndex[start]] = A[idxA];
+                idxB++;
+                start++;
+            } else {
+                res[bIndex[end]] = A[idxA];
+                end--;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Using PriorityQueue
+     */
+    public static int[] advantageCount3(int[] A, int[] B) {
+        if (A == null || B == null || A.length != B.length) return new int[0];
+
+        Arrays.sort(A);
+
+        final int len = A.length;
+        // added by decreasing order
+        final PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        for (int i = 0; i < len; i++) {
+            pq.add(new int[]{B[i], i});
+        }
+
+        final int[] res = new int[len];
+        int lo = 0, hi = len - 1;
+        while (!pq.isEmpty()) {
+            int[] arr = pq.poll();
+            int val = arr[0];
+            int idx = arr[1];
+            if (A[hi] <= val) {
+                res[idx] = A[lo];
+                lo++;
+            } else {
+                res[idx] = A[hi];
+                hi--;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * Using BinarySearch
+     */
+    public static int[] advantageCount4(int[] A, int[] B) {
+        if (A == null || B == null || A.length != B.length) return new int[0];
+
+        Arrays.sort(A);
+
+        final int len = A.length;
+        final int[] res = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            res[i] = binarySearch(A, B[i]);
+        }
+        return res;
+    }
+
+    /**
+     * Return the smallest unused index i in A that A[i] > target,
+     * the smallest unused index i from 0 otherwise
+     * <p>
+     * Marked A[i] as visited by flip all the bits (~) of A[i]
+     */
+    private static int binarySearch(final int[] A, final int target) {
+        final int len = A.length;
+        int lo = 0, hi = len - 1;
+
+        while (lo < hi) {
+            int mid = lo + ((hi - lo) >> 1);
+
+            if ((A[mid] >= 0 ? A[mid] : ~A[mid]) <= target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        if (A[lo] > target) {
+            while (lo < len && A[lo] < 0) lo++;
+            if (lo < len) {
+                A[lo] = ~A[lo];
+                return ~A[lo];
+            }
+        }
+
+        lo = 0;
+        while (lo < len && A[lo] < 0) lo++;
+        A[lo] = ~A[lo];
+        return ~A[lo];
+    }
+
+    /**
+     * Using TreeMap
+     */
+    public static int[] advantageCount5(int[] A, int[] B) {
+        if (A == null || B == null || A.length != B.length) return new int[0];
+
+        final int len = A.length;
+        final TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i : A) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        final int[] res = new int[len];
+        for (int i = 0; i < len; i++) {
+            Integer x = map.higherKey(B[i]);
+            if (x == null) x = map.firstKey();
+
+            if (map.get(x) == 1) map.remove(x);
+            else map.put(x, map.get(x) - 1);
+            res[i] = x;
+        }
+        return res;
+    }
+}
