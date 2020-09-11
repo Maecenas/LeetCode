20e19e34b5cc24da681ea0098510167772468d3d
Maecenas Li <lx70716@gmail.com>
2020-09-12 00:20:14 +0800

    ✨ feat: Add ListNode array constructor

--- src/main/java/LeetCode/utils/ListNode.java
+++ src/main/java/LeetCode/utils/ListNode.java
@@ -9,6 +9,15 @@ public class ListNode {
         val = x;
     }
 
+    public ListNode(int... values) {
+        ListNode curr = this;
+        curr.val = values[0];
+        for (int i = 1; i < values.length; i++) {
+            curr.next = new ListNode(values[i]);
+            curr = curr.next;
+        }
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder();
